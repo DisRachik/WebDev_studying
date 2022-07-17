71068c14c22b2548e66a1dee33c7a436bfc9333e
DisRachik <radchenkoig@gmail.com>
2022-07-17 15:34:15 +0300

    feat: add carousel, implement adaptation slider

---
+++ Pulse/src/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/pointer_to_left.svg" /></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/pointer_to_right.svg" /></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
+  });
+  
+  
+});
+
+
