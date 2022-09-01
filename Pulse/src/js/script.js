e35592caabcd6376d7d98bc54627ee976da8b569
DisRachik <radchenkoig@gmail.com>
2022-09-01 23:12:47 +0300

    feat: Smoth scroll up

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -118,4 +118,19 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  // Smoth scroll - icon up
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 900) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  })
 });
